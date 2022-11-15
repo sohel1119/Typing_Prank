3f5c751b8fd30bd52bce86998f2fd67914e9ecd5
sohel1119 <94044610+sohel1119@users.noreply.github.com>
2022-11-15 14:23:51 +0530

    Update typing_prank.py

--- typing_prank.py
+++ typing_prank.py
@@ -5,5 +5,5 @@ animal = ('Dog', 'Monkey', 'Cow', 'Buffalow', 'Elephant', 'Donkey', 'Cat',)
 time.sleep(10)
 for i in range (100):
  a = random.choice(animal)
- pg.write('You Are a' + a)
+ pg.write('You Are a ' + a)
  pg.press('enter')
